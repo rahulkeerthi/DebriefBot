6d67a072c114533926eaf702547a7075237c438b
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-09-23 01:01:00 +0100

    slack bot setup with bolt

--- app.js
+++ app.js
@@ -6,9 +6,39 @@ const app = new App({
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
 })
 
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }) => {
+	// say() sends a message to the channel where the event was triggered
+	await say({
+		blocks: [
+			{
+				type: "section",
+				text: {
+					type: "mrkdwn",
+					text: `Hey there <@${message.user}>!`,
+				},
+				accessory: {
+					type: "button",
+					text: {
+						type: "plain_text",
+						text: "Click Me",
+					},
+					action_id: "button_click",
+				},
+			},
+		],
+		text: `Hey there <@${message.user}>!`,
+	})
+})
+
+app.action("button_click", async ({ body, ack, say }) => {
+	// Acknowledge the action
+	await ack()
+	await say(`<@${body.user.id}> clicked the button`)
+})
 ;(async () => {
 	// Start your app
-	await app.start(process.env.PORT || 3000)
-
+	await app.start(process.env.PORT || 4390)
+	console.log(process.env.PORT)
 	console.log("⚡️ Bolt app is running!")
 })()
